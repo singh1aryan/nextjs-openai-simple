43725beb20d5bab359928603a9a3eece03b2db77
Aryan Singh <aryansingh@MacBook-Pro.home>
2023-07-20 23:49:59 -0700

    readme

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    chatGpt: 'https://api.openai.com/v1/chat/completions',
+    apiKey: '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',
+  },
   reactStrictMode: true,
 }
 
